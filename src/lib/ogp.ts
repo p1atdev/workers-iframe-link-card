b8362f3300697e5e667c279501f2d1ecbf3b45ec
Plat <60182057+p1atdev@users.noreply.github.com>
2025-08-27 02:25:07 +0900

    feat: support favicon, validate urls, check images exists

--- src/lib/ogp.ts
+++ src/lib/ogp.ts
@@ -9,34 +9,74 @@ export interface OGPData {
   url?: string | null;
   siteName?: string | null;
   type?: string | null;
+  favicon?: string | null;
 }
 
 class OGPParser {
+  origin: string = "";
   ogp: OGPData = {};
 
-  element(element: Element) {
+  constructor(url: string) {
+    this.origin = new URL(url).origin;
+  }
+
+  meta(element: Element) {
     const property = element.getAttribute("property");
     const content = element.getAttribute("content");
 
     switch (property) {
-      case "og:title":
+      case "og:title": {
         this.ogp.title = content;
         break;
-      case "og:description":
+      }
+      case "og:description": {
         this.ogp.description = content;
         break;
-      case "og:image":
-        this.ogp.image = content;
+      }
+      case "og:image": {
+        this.ogp.image = new URL(content || "", this.origin).href;
         break;
-      case "og:url":
-        this.ogp.url = content;
+      }
+      case "og:url": {
+        this.ogp.url = new URL(content || "", this.origin).href;
         break;
-      case "og:site_name":
+      }
+      case "og:site_name": {
         this.ogp.siteName = content;
         break;
-      case "og:type":
+      }
+      case "og:type": {
         this.ogp.type = content;
         break;
+      }
+    }
+  }
+
+  link(element: Element) {
+    const rel = element.getAttribute("rel");
+    const href = element.getAttribute("href");
+
+    switch (rel) {
+      case "icon":
+      case "shortcut icon": {
+        this.ogp.favicon = new URL(href || "", this.origin).href;
+        break;
+      }
+    }
+  }
+
+  element(element: Element) {
+    const tagName = element.tagName.toLowerCase();
+
+    switch (tagName) {
+      case "meta": {
+        this.meta(element);
+        break;
+      }
+      case "link": {
+        this.link(element);
+        break;
+      }
     }
   }
 }
@@ -60,14 +100,46 @@ export async function getOGP(url: string): Promise<OGPData> {
     throw new Error(`The URL ${url} points to an image, not a webpage.`);
   }
 
-  const ogpParser = new OGPParser();
+  const ogpParser = new OGPParser(url);
 
   // https://github.com/oven-sh/bun/issues/4408#issuecomment-1736976282
-  const _res = new HTMLRewriter().on("meta", ogpParser).transform(res);
+  const _res = new HTMLRewriter().on("meta, link", ogpParser).transform(res);
   await _res.text();
 
   // パースしたものを取得
   const ogp = ogpParser.ogp;
+
+  // check ogp image exists
+  if (ogp.image) {
+    const res = await fetch(ogp.image, {
+      method: "HEAD",
+      headers: {
+        "User-Agent": userAgent,
+      },
+      redirect: "follow",
+    });
+
+    // 適切に取得できなかったら削除
+    if (!res.ok) {
+      delete ogp.image;
+    }
+  }
+
+  // check favicon
+  if (ogp.favicon) {
+    const res = await fetch(ogp.favicon, {
+      method: "HEAD",
+      headers: {
+        "User-Agent": userAgent,
+      },
+      redirect: "follow",
+    });
+
+    // 適切に取得できなかったら削除
+    if (!res.ok) {
+      delete ogp.favicon;
+    }
+  }
 
   return ogp;
 }
